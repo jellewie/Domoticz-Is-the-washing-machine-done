72b9ac31a79493acc836adc9f89871cf2d0763a8
JelleWie <1545223+jellewie@users.noreply.github.com>
2021-03-17 20:23:12 +0100

    Added readme

--- Vbscript.lua
+++ Vbscript.lua
@@ -1,23 +1,16 @@
 --"Is the washing machine done?"
---	Version 2.0 https://www.domoticz.com/forum/viewtopic.php?t=23798
---	Version 3.0 Tweaked by JelleWho https://github.com/jellewie
---TLDR: You need a smart Switch to turn a device off/on, AND you need a smart kWh measureing device, to setup read intresting lines with █ in them
-
---This script checks if a switch has been turned on, or the device is using more power than 'StandbyMaxWatt'
---'TimeOut' minutes after the switch (and the device) has been turned on, the code start to check if the machine is done
---If the switch is turned off, or the device uses 0Watt, or it uses less than 'StandbyMaxWatt' average over 'TimeOut' minutes, The Switch (and the device) is turned off
---This is usefull so you can attach a notification when the switch turns off (and thus when the device is done), This is extreemly usefull for washing machines, dryers, and 3D printers for example
+--Code Tweaked by JelleWho https://github.com/jellewie/Domoticz-Is-the-washing-macgine-done--
 
 local DEVICES = {
 --█Change the below values to EXACTLY match the names set in Domoticz as device names
-    --Random,  SwitchName,  MeterName             , TimeOut, StandbyMaxWatt
-	['a'] = {'Wasdroger' , 'SC-Wasdroger gebruik' , 5      , 1},
-	['b'] = {'Wasmachine', 'SC-Wasmachine gebruik', 5      , 4},
-	--['c'] = {'3D_Printer', 'SC-3D_Printer gebruik', 5      , 12},					--Another example
+    --Random,  SwitchName,  MeterName		 , TimeOut, StandbyMaxWatt
+	['a'] = {'Wasmachine', 'Wasmachine usage', 5      , 4},
+	['b'] = {'Wasdroger' , 'Wasdroger usage' , 5      , 1},
+	--['c'] = {'3D_Printer', '3D_Printer usage', 5      , 12},						--Another example
 	--Also add these names to "data = {" in the format of "['SwitchName'] = {history = true, maxMinutes = 10}," !
 }
 
-local LogDebugging = false						                                    --Set to TRUE to recieve more information in the log, This includes most values of each status check
+local LogDebugging = false						                                    --Set to TRUE to receive more information in the log, this includes most values of each status check
 local LogChecking = true						                                    --Set to TRUE to log what the result of the check was (machine was on/off/idle etc)
 
 return {
@@ -30,8 +23,8 @@ return {
 	},
 	data = {
 --█use exact SwitchName to match DEVICES
-		['Wasdroger']  = {history = true, maxMinutes = 10},                         --Log the values and store them here, remove all data after 10 min
 		['Wasmachine'] = {history = true, maxMinutes = 10},
+		['Wasdroger']  = {history = true, maxMinutes = 10},                         --Log the values and store them here, remove all data after 10 min
 		--['3D_Printer'] = {history = true, maxMinutes = 10},
 	},
 --You can stop reading now, from here on out its just code
@@ -90,7 +83,7 @@ return {
 				return('Switching On: Act_Power>'..tostring(StandbyMaxWatt))
 			end
 			--Note: switch and machine are not active
-			if power_average > 0 then 								                --Switch is off but average is not reseted
+			if power_average > 0 then 								                --Switch is off but average is not reset
 				domoticz.data[DevSwitch].reset()                                    --Reset history (and the new average of NULL data is ofc 0)
 			end 
 			return('Off')											                --Device is off
@@ -98,8 +91,7 @@ return {
 
 		for i, machine in pairs(DEVICES) do 						                --Loop thru all the devices
 			checked = status(machine)								                --Check the status of each device
-			if LogChecking then domoticz.log('Status of '..machine[1]..'='..checked) end	--Log the status of each device
+			if LogChecking then domoticz.log('Status of '..machine[1]..'='..checked) end --Log the status of each device
 		end
 	end
 }
--- Some info about the DzVents language used here https://www.domoticz.com/wiki/DzVents:_next_generation_Lua_scripting
---
+++ readme.md
@@ -0,0 +1,24 @@
+# Is the washing machine done?
+This script checks if a switch has been turned on, or the device is using more power than 'StandbyMaxWatt'.
+'TimeOut' minutes after the switch (and the device) has been turned on, the code start to check if the machine is done.
+If the switch is turned off, or the device uses 0Watt, or it uses less than 'StandbyMaxWatt' average over 'TimeOut' minutes, The Switch (and the device) is turned off.
+This is useful so you can attach a notification when the switch turns off (and thus when the device is done), This is extremely useful for washing machines, dryers, and 3D printers for example
+
+# Setup
+1. Buy a smart Switch to turn a device off/on, AND a smart kWh measuring device (Can be both at once like the 'WR01ZE' plug (zwave stick required))
+2. Add to Domoticz, and check both are working
+3. Go to Setup/Devices in Domoticz
+
+4a. Rename the switch 'Light/Switch - Switch' to the set 'SwitchName' (These are defined in the scipt 'Vbscript.lua' in the 'DEVICES' declaration)
+4b. Rename the meter with 'Usage - Electric in Watt' to the set 'MeterName'
+
+As the point of writing this, the SwitchName also need to be declaired in 'data' seperatly
+
+5. Redo the last 2 steps with different names if more than one device
+6. Add the code in Setup/More options/Events and then add(+) dzVents. just copy the text in 'Vbscript.lua' into the field
+
+
+# Credits and links
+Credits to the old Version 2.0 made by @felix63 https://www.domoticz.com/forum/viewtopic.php?t=23798
+
+Some info about the DzVents language used here https://www.domoticz.com/wiki/DzVents:_next_generation_Lua_scripting
